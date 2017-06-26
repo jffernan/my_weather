40687e81b53a328f7e7272f0d905065993462abf
James Fernandez <jffernan@comcast.net>
2017-06-26 17:55:05 +0000

    Defined menu with input code for CLI in cli.rb

--- lib/my_weather/cli.rb
+++ lib/my_weather/cli.rb
@@ -10,5 +10,20 @@ class MyWeather::CLI
     puts "*******Welcome to my Weather CLI app where you can get current weather conditions*******"
   end
 
-  
+  def menu
+    input = ""
+      while input != "exit"
+        puts "What is your 'zip code'?  Enter a 5-digit 'zip code' or type 'exit' to leave the program."
+        input = gets.strip.downcase
+        if input.length == 5
+          puts "display_report"
+          zip = input.to_i
+        elsif input =="exit"
+          ending
+        else
+          puts "Please enter five numbers for 'zip code' or type ‘exit’ to quit."
+        end
+      end
+    end
+
 end
